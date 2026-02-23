09cf0ff97f676389cc1f8f5263c045b46cf5f95a
snail <sgefsnkiospch908@gmail.com>
2026-02-23 10:08:54 +0300

    job

---
+++ app/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Строка подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
+engine = create_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+SessionLocal = sessionmaker(bind=engine)
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import (AsyncSession, async_sessionmaker,
+                                    create_async_engine)
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для SQLite
+DATABASE_URL = (
+    "postgresql+asyncpg://postgres:postgres@localhost:5431/ecommerce_db"
+)
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(
+    async_engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+class Base(DeclarativeBase):
+    pass
